f86fb665ce10ad95a61a8e1d4b01bfc8224e21a6
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-06-28 08:07:58 +0300

    scope levels

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// helpers.js
+
+// Declare customerName globally
+window.customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+// Constant variable for least favorite customer
+const leastFavoriteCustomer = 'someone';
+
+// Function to change least favorite customer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This should throw an error
+}
+
+// Export functions if using Node.js or similar environment
